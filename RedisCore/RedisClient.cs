8d631991b42e7ba3d376022e3f9aea49b6780303
Mert Unaler <Mert.Unaler@logo.com.tr>
2023-09-03 16:15:48 +0300

    +RedisClient implemented.

---
+++ RedisCore/RedisClient.cs
@@ -0,0 +1,37 @@
+﻿using StackExchange.Redis;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace RedisCore
+{
+    public class RedisClient : IDisposable
+    {
+        public string Channel1 = "John Doe";
+        public string Channel2 = "Jane Doe";
+        public ConnectionMultiplexer _conn;
+        private bool Disposed;
+        public RedisClient()
+        {
+            var config = new ConfigurationOptions()
+            {
+                EndPoints = { "localhost:1923" },
+                Password = "mert11",
+                Ssl = true
+            };
+            //Connection to the Redis Server.
+            _conn = ConnectionMultiplexer.Connect(config);  
+        }
+
+
+        public ISubscriber GetSubscriber()
+        {
+            //Get a connection 
+            return _conn.GetSubscriber();
+        }
+
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+    }
+}
